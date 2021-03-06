257e2945e95c5e8d7c8bd42bfe1a31fe124915f7
Matheus-Alpe <matttalves@gmail.com>
2021-03-06 20:58:29 -0300

    add: test to operations

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 module.exports = {
-  sum() {},
-  sub() {},
-  mult() {},
-  div() {},
+  sum(v1, v2) {
+    return v1 + v2;
+  },
+  sub(v1, v2) {
+    return v1 - v2;
+  },
+  mult(v1, v2) {
+    return v1 * v2;
+  },
+  div(v1, v2) {
+    return (v2 === 0) ? 'Não é possível divisão por zero' : v1 / v2
+  },
 };
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -33,4 +33,36 @@ describe('Calc', () => {
       expect(calc.div).to.be.a('function');
     });
   });
+
+  describe('Sum', () => {
+    it('should return 4 when `sum(2, 2)`', () => {
+      expect(calc.sum(2, 2)).to.be.equal(4);
+    });
+  });
+
+  describe('Sub', () => {
+    it('should return 4 when `sub(6, 2)`', () => {
+      expect(calc.sub(6, 2)).to.be.equal(4);
+    });
+
+    it('should return -4 when `sub(6, 10)`', () => {
+      expect(calc.sub(6, 10)).to.be.equal(-4);
+    });
+  });
+
+  describe('Mult', () => {
+    it('should return 12 when `mult(6, 2)`', () => {
+      expect(calc.mult(6, 2)).to.be.equal(12);
+    });
+  });
+
+  describe('Div', () => {
+    it('should return 3 when `div(6, 2)`', () => {
+      expect(calc.div(6, 2)).to.be.equal(3);
+    });
+
+    it('should return `Não é possível divisão por zero` when divide by 0', () => {
+      expect(calc.div(4, 0)).to.be.equal('Não é possível divisão por zero');
+    });
+  });
 });
