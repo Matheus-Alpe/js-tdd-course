09df784e9fc4b4cda9e2e37f7f642c9ae58da0ac
Matheus-Alpe <matttalves@gmail.com>
2021-03-07 13:44:57 -0300

    add: FizzBuzz

--- src/main.js
+++ src/main.js
@@ -1,14 +1,8 @@
-const sum = (v1, v2) => v1 + v2;
-
-const sub = (v1, v2) => v1 - v2;
-
-const mult = (v1, v2) => v1 * v2;
-
-const div = (v1, v2) => ((v2 === 0) ? 'Não é possível divisão por zero' : v1 / v2);
-
-export {
-  sum,
-  sub,
-  mult,
-  div,
+const FizzBuzz = (num) => {
+  if (num % 3 === 0 && num % 5 === 0) return 'FizzBuzz';
+  if (num % 3 === 0) return 'Fizz';
+  if (num % 5 === 0) return 'Buzz';
+  return num;
 };
+
+export default FizzBuzz;
