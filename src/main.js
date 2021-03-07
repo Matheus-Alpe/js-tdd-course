3f62ed797b1f478330f3291bc05ee5e38b1d3d7f
Matheus-Alpe <matttalves@gmail.com>
2021-03-07 11:55:44 -0300

    feat: add babel and convert to ES6

--- src/main.js
+++ src/main.js
@@ -1,17 +1,14 @@
-module.exports = {
-  sum(v1, v2) {
-    return v1 + v2;
-  },
+const sum = (v1, v2) => v1 + v2;
 
-  sub(v1, v2) {
-    return v1 - v2;
-  },
+const sub = (v1, v2) => v1 - v2;
 
-  mult(v1, v2) {
-    return v1 * v2;
-  },
+const mult = (v1, v2) => v1 * v2;
 
-  div(v1, v2) {
-    return (v2 === 0) ? 'Não é possível divisão por zero' : v1 / v2;
-  },
+const div = (v1, v2) => ((v2 === 0) ? 'Não é possível divisão por zero' : v1 / v2);
+
+export {
+  sum,
+  sub,
+  mult,
+  div,
 };
