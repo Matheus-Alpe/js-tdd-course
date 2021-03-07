9696c5e4677a8803a379fb8e6a6d22a94a50bd15
Matheus-Alpe <matttalves@gmail.com>
2021-03-06 21:04:25 -0300

    test 2

--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@ module.exports = {
   sum(v1, v2) {
     return v1 + v2;
   },
+
   sub(v1, v2) {
     return v1 - v2;
   },
+
   mult(v1, v2) {
     return v1 * v2;
   },
+
   div(v1, v2) {
     return (v2 === 0) ? 'Não é possível divisão por zero' : v1 / v2;
   },
